ed8348ae23896a15d9af42cac17366e7ff53b999
francia garcia <franciagarcia09@gmail.com>
2023-12-15 09:55:02 -0800

    add moo after each word and capitalize the words in textbox

--- fancifymytext.js
+++ fancifymytext.js
@@ -6,10 +6,14 @@ var boringBettyRadioo = document.getElementById("boringBetty");
     if (fancyShmancyRadio.checked) {
         // Apply styles for "FancyShmancy"
         boxArea.style.fontWeight = "bold";
+        boxArea.style.color= "blue";
+        boxArea.style.textDecoration = "underline";
         
     } 
     else if(boringBettyRadioo.checked){
         boxArea.style.fontWeight = "normal";
+        boxArea.style.fontcolor= "";
+        boxArea.style.textDecoration = "none";
 
     }    else {
         
@@ -17,6 +21,23 @@ var boringBettyRadioo = document.getElementById("boringBetty");
         
     }
 
+}
+function handleMooButtonClick(){
+    var boxArea = document.getElementById("UserInput");
+    var textVal =boxArea.value;
+
+    
+    var words = textVal.split( " ");
+
+    for (var i=0; i< words.length; i++ ){
+        var wordArray = words[i].split("");
+        wordArray[wordArray.length -1] +="-Moo";
+        words[i] = wordArray.join("");
+        
+    }
+    var newText = words.join(" ");
+    newText = newText.toUpperCase();
+    boxArea.value = newText;
 }
 function handleBiggerButtonClick() {
     alert('Hello, World!');
