6375ea440b80a9cdf38b290d32df29099cd52f96
francia garcia <franciagarcia09@gmail.com>
2023-12-15 07:52:06 -0800

    push changes font

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,3 +1,24 @@
-void handleBiggerButtonClick(){
-     alert("Hello, World!");
+function radioHandleOnChange(){
+
+var boxArea = document.getElementById("UserInput");
+ var fancyShmancyRadio = document.getElementById("fancyShmancy");
+var boringBettyRadioo = document.getElementById("boringBetty");
+    if (fancyShmancyRadio.checked) {
+        // Apply styles for "FancyShmancy"
+        boxArea.style.fontWeight = "bold";
+        
+    } 
+    else if(boringBettyRadioo.checked){
+        boxArea.style.fontWeight = "normal";
+
+    }    else {
+        
+       //do nothing
+        
+    }
+
+}
+function handleBiggerButtonClick() {
+    alert('Hello, World!');
+    document.getElementById("UserInput").style.fontSize = "24pt";
 }
